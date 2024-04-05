3a5441ea86f939b34c1cc6ff68459082d9333aad
abs-09 <absal2009@live.com>
2024-04-05 23:54:39 +0500

    worked on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,26 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="p-12">
+        {{-- statistics --}}
+        <div>
+            <h3 class="text-xl font-semibold leading-6 text-gray-700">Our Reach Across Male' City, Maldives</h3>
+            <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+                <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+                    <dt class="truncate text-sm font-medium text-gray-500">Citizens Delivered to Daily</dt>
+                    <dd class="mt-1 text-3xl font-semibold tracking-tight text-pink-700">71,897</dd>
                 </div>
-            </div>
+                <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+                    <dt class="truncate text-sm font-medium text-gray-500">Number of Registered Donators</dt>
+                    <dd class="mt-1 text-3xl font-semibold tracking-tight text-purple-700">58.16%</dd>
+                </div>
+                <div class="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+                    <dt class="truncate text-sm font-medium text-gray-500">Number of Registered Distributors</dt>
+                    <dd class="mt-1 text-3xl font-semibold tracking-tight text-indigo-700">24.57%</dd>
+                </div>
+            </dl>
+        </div>
+        {{-- Image --}}
+        <div class="py-5">
+            <img class="rounded h-72 w-full object-cover" src="https://images.unsplash.com/photo-1594582280467-339a2fa5f9c8?q=80&w=2533&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
         </div>
     </div>
 </x-app-layout>
